755af00dbccc98333642a21620fb1d7489346bee
Ansh <158754607+An-s-h@users.noreply.github.com>
2024-08-15 00:41:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require("./config/conn");
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL || "http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL || "https://super-pod.vercel.app"],
     credentials: true,
   })
 );
